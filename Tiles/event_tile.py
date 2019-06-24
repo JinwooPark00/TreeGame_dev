d4e37f14c0d6f23a2d776e902d434d5b6b61f21f
JinwooPark00 <jinwoopark00@gmail.com>
2019-06-24 13:58:53 -0700

    worked on main

--- Tiles/event_tile.py
+++ Tiles/event_tile.py
@@ -3,12 +3,13 @@ import random
 
 
 class EventTile(Tile):
-    #(water, light); add more events
-    events = {'Spring': {'Flood': (3, -1)}, 'Summer': {'Drought': (-3, 2), 'Sea Levels Rise': (2, 0)}, 'Fall': {}, 'Winter': {'Snowstorm': (-1, -1)}}
+    # (water, light); add more events
+    events = {'Spring': {'Flood': (3, -1)}, 'Summer': {'Drought': (-3, 2), 'Sea Levels Rise': (2, 0)}, 'Fall': {},
+              'Winter': {'Snowstorm': (-1, -1)}}
 
     def __init__(self):
         super(EventTile, self).__init__()
-        self.color = 'Green'
+        self.color = (0, 255, 0)
 
     def pick_event(self, player):
         s = EventTile.events[self.season]
--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 import sys, pygame
 from board import Board
 
-pygame.init()
-
-screen = pygame.display.set_mode((960, 960))
 print("hi")
 
 if __name__ == '__main__':
-    board = Board()
+    pygame.init()
+    screen = pygame.display.set_mode((480, 480))
+    players = {}
+    board = Board(players)
+    x = 0
+    y = 0
+    once = True
+
+    # pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(x, y, 100, 100), 0)
+    # for index, tile in enumerate(board.board):
+    #     print("drawing board")
+    #     # pygame.draw.rect(screen, tile.color, pygame.Rect(x, y, 20, 20), 0)
+    #     # if index < 13:
+    #     #     x += 20
+    #     # elif index < 25:
+    #     #     y += 20
+    #     # elif index < 37:
+    #     #     x -= 20
+    #     # else:
+    #     #     y -= 20
+
     while 1:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-        for tile in board.board:
-            pygame.draw.rect(screen, (20,20,20), )
-
-
-
+        if once:
+            for index, tile in enumerate(board.board):
+                print("drawing board")
+                pygame.draw.rect(screen, tile.color, pygame.Rect(x, y, 100, 100), 5)
+                if index < 13:
+                    x += 20
+                elif index < 25:
+                    y += 20
+                elif index < 37:
+                    x -= 20
+                else:
+                    y -= 20
+            once = False
 
 # if __name__ == '__main__':
 #     board = Board()
